b1553f6a62db58e07e239adcf8f3153393a75360
mahediali <mahedialisatkar110@gmail.com>
2022-11-20 11:36:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/practical7_20012011067/AlarmBroadcastReceiver.kt
@@ -0,0 +1,16 @@
+package com.example.practical7_20012011067
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.media.MediaPlayer
+
+class AlarmBroadcastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        var mp: MediaPlayer? = null
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("MyReceiver.onReceive() is not implemented")
+
+    }
+}
